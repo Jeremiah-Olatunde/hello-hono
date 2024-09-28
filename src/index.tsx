f49d0b417d2d3c6c0575a7b178a9a65e1b4ae03d
Jeremiah Olatunde <jesuseun.j.olatunde@gmail.com>
2024-09-28 20:02:30 +0100

    feat: add jsx rendering

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,5 @@
 import { Hono } from "hono";
+import { Poem } from "./Poem";
 
 const app = new Hono();
 
@@ -17,6 +18,10 @@ app.get("user/post", (context) => {
 app.get("/user/:id{\\d+}", (context) => {
 	const id = context.req.param("id");
 	return context.text(`USER(${id})`);
+});
+
+app.get("/poem", (context) => {
+	return context.html(<Poem></Poem>);
 });
 
 export default app;
