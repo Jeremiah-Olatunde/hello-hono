41923fd764d45ecffffb0ba024f4262f5a157764
Jeremiah Olatunde <jesuseun.j.olatunde@gmail.com>
2024-09-28 19:43:24 +0100

    feat: add path and query params

--- src/index.ts
+++ src/index.ts
@@ -7,4 +7,16 @@ app.get("/", async (context) => {
 	return context.html(html);
 });
 
+app.get("user/post", (context) => {
+	const page = context.req.query("page");
+	const limit = context.req.query("limit");
+
+	return context.text(`POST(page: ${page} | limit: ${limit})`);
+});
+
+app.get("/user/:id{\\d+}", (context) => {
+	const id = context.req.param("id");
+	return context.text(`USER(${id})`);
+});
+
 export default app;
